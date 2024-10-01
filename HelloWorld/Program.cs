646d06f55d0722c68088922f0ed0a4af7b8d643a
Brendan Lenfesty <Lenfesty-B@ulster.ac.uk>
2024-10-01 11:28:52 +0100

    updated

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Wow");
-//this works because i added a comment
+﻿Main();
+static void Main()
+{
+
+
+    Console.WriteLine("Enter string");
+    string str = Console.ReadLine();
+    int numberOfWords = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        char c = str[i];
+        if (Char.IsUpper(c))
+        {
+            // Add 1 to the total word count
+            numberOfWords++;
+        }
+
+    }
+    Console.WriteLine($"The sentence you inputted is: {str}");
+    Console.WriteLine($"Number of words = {numberOfWords}");
+    //this works because i added a comment
+
+}
